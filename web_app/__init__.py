c609c829537806cbb7152f8195aa2c8249014683
Griffinw15 <wilson.griffin15@gmail.com>
2020-05-28 10:55:13 -0700

    close to deployed

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,8 @@
 # web_app/__init__.py
 
 from flask import Flask
-
+import os
+from dontenv import load_dotenv
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
@@ -9,13 +10,21 @@ from web_app.routes.stats_routes import stats_routes
 #from web_app.routes.book_routes import book_routes
 
 #DATABASE_URI = "sqlite:///web_app_99.db" # using relative filepath
-DATABASE_URI = "sqlite:////Users/griffinwilson/Desktop/Web_app_twitoff2.0/Web_app_twitoff.db" # using absolute filepath on Mac (recommended)
+##DATABASE_URL = "sqlite:////Users/griffinwilson/Desktop/Web_app_twitoff2.0/Web_app_twitoff.db" # using absolute filepath on Mac (recommended)
 #DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="it's a secret..")
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SECRET_KEY"] = SECRET_KEY
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
